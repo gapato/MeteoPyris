59afe7b67771489111e23d0d64081d623854a3ad
Gaspard Jankowiak <gaspard@oknaj.eu>
2013-02-24 08:29:38 +0100

    Measures: allow dump to JSON for ajax loading

--- MeteoPyris/measures.py
+++ MeteoPyris/measures.py
@@ -27,6 +27,10 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import json
+from json import encoder
+encoder.FLOAT_REPR = lambda o: format(o, '.2f')
+
 class Measures:
 
     def __init__(self, cursor, sql_fields):
@@ -44,11 +48,73 @@ class Measures:
 
             self.data.append(row)
 
-        self.data.reverse()
         cursor.connection.commit()
 
     def length(self):
         return len(self.data)
+
+    def _prefilter_data(self, window_width=1):
+
+        d = None
+        date_start = date_end = None
+
+        avg = {}
+        post_filter_dict = {}
+
+        keys = self.data[0].keys()
+        keys.remove('time')
+
+        for k in keys:
+            avg[k] = 0
+            post_filter_dict['%s_DATA' % k.upper()] = []
+
+        i = 0
+        n = 0
+        for row in self.data:
+
+            if n == 0:
+                date_start = row['time']
+                n = 1
+
+            i += 1
+
+            for k in keys:
+                avg[k] = avg[k]*(i-1)/i + row[k]/i
+
+            if window_width == 1 or i == window_width//2:
+                date_end = d = row['time']
+
+            if i < window_width:
+                continue
+            else:
+                i = 0
+                for k in keys:
+                    post_filter_dict['%s_DATA' % k.upper()].append(round(avg[k],2))
+                    avg[k] = 0
+
+        post_filter_dict['START_DATE'] = date_start
+        post_filter_dict['END_DATE'] = date_end
+
+        return post_filter_dict
+
+    def serialize_json(self, filename=None, window_width=1):
+
+        assert self.length() > 0, 'Measure set is empty, cannot render'
+
+        filtered_data = self._prefilter_data(window_width)
+
+        obj_to_serial = filtered_data.copy()
+
+        obj_to_serial['START_DATE'] = obj_to_serial['START_DATE'].isoformat()
+        obj_to_serial['END_DATE']   = obj_to_serial['END_DATE'].isoformat()
+
+        obj_to_serial['TIME_STEP'] = 1000 * 60 * 5 * window_width
+
+        if filename:
+            with open(filename, 'w') as fd:
+                json.dump(obj_to_serial, fd)
+        else:
+            return json.dumps(obj_to_serial)
 
     def render_template(self, filename, window_width=1):
 
@@ -59,61 +125,22 @@ class Measures:
         except:
             raise ImportError('MeteoPyris needs the pystache package to render templates')
 
-
-        d = None
-        d_start = d_end = None
-
-        avg = {}
-        datastrings = {}
-
-        keys = self.data[0].keys()
-        keys.remove('time')
-
-        for k in keys:
-            avg[k] = 0
-            datastrings[k] = ''
-
-        i = 0
-        n = 0
-        for row in self.data:
-
-            # remember first and last date (we are filtering backwards, ORDER BY time DESC)
-            if n == 0:
-                d_end = row['time']
-                n = 1
-
-            i += 1
-
-            for k in keys:
-                avg[k] = avg[k]*(i-1)/i + row[k]/i
-
-            if window_width == 1 or i == window_width//2:
-                d_start = d = row['time']
-
-            if i < window_width:
+        filtered_data = self._prefilter_data(window_width)
+        ds = filtered_data['START_DATE']
+        de = filtered_data['END_DATE']
+        template_context = {
+            'START_DATE' : 'Date.UTC(%d, %d, %d, %d ,%d, 0)' % (ds.year, ds.month-1, ds.day, ds.hour, ds.minute),
+            'END_DATE'   : 'Date.UTC(%d, %d, %d, %d ,%d, 0)' % (de.year, de.month-1, de.day, de.hour, de.minute),
+            'TIME_STEP'  : '%d' % (1000 * 60 * 5 * window_width)
+            }
+        for k in filtered_data.keys():
+            if k == 'START_DATE' or k == 'END_DATE':
                 continue
-            else:
-                i = 0
-                for k in keys:
-                    datastrings[k]  = "%.1f," % (avg[k]) + datastrings[k]
-                    avg[k] = 0
-
-        if n > 0:
-            for k in keys:
-                datastrings[k] = datastrings[k][:-1]
-            d_start = "Date.UTC(%d,  %d, %d, %d, %d, %d)" % (d_start.year, d_start.month-1, d_start.day, d_start.hour, d_start.minute, 0)
-            d_end   = "Date.UTC(%d,  %d, %d, %d, %d, %d)" % (d_end.year, d_end.month-1, d_end.day, d_end.hour, d_end.minute, 0)
+            template_context[k] = ','.join(map(str, filtered_data[k]))
 
         r = ''
         renderer = pystache.Renderer(file_encoding='utf-8', string_encoding='utf-8')
         with open(filename, 'r') as template:
             for line in template:
-                template_context = {
-                    'START_DATE' : d_start,
-                    'END_DATE'   : d_end,
-                    'TIME_STEP'  : '%d' % (1000 * 60 * 5 * window_width)
-                    }
-                for k in keys:
-                    template_context['%s_DATA' % k.upper()] = datastrings[k]
                 r += renderer.render(line, template_context)
         return r
